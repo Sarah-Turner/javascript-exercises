f30797ff57703ffbcc22c79f85ce94fdbe380206
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-11 15:39:51 -0800

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let value of args) {
+        while (array.includes(value)) {
+            array.splice(array.indexOf(value), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
