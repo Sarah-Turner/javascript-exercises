9ee48806af7d2dec9b47d52037f7c985693ab769
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-11 16:08:16 -0800

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstValue, secondValue) {
+    // check for negatives, floating points, and non-numbers to return "ERROR"
+    if (typeof firstValue !== "number" || typeof secondValue !== "number") {
+        return "ERROR";
+    }
+    if (firstValue < 0 || secondValue < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(firstValue) || !Number.isInteger(secondValue)) {
+        return "ERROR";
+    }
+    let start = (firstValue < secondValue) ? firstValue : secondValue;
+    let end = (firstValue > secondValue) ? firstValue : secondValue;
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
