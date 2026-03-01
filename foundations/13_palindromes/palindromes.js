4699cfa1a7bbfa1f7b5827cfa4123e51c90eb47e
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-02-28 19:02:23 -0800

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let left = 0;
+    let right = string.length - 1;
 
+    while (left < right) {
+        while (!isAlNum(string[left])) {
+            left++
+        }
+        while (!isAlNum(string[right])) {
+            right--;
+        }
+
+        if (string[left].toLowerCase() !== string[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 };
+
+function isAlNum(c) {
+    return ("0" <= c && c <= "9" || "a" <= c && c <= "z" || "A" <= c && c <= "Z")
+}
 
 // Do not edit below this line
 module.exports = palindromes;
