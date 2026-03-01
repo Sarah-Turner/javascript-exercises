197b2e0a832d48381845324f58222ddeb3450fc8
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-03-01 10:05:40 -0800

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) {
+        return "OOPS";
+    }
 
+    let sequence = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        sequence.push(sequence[i-1] + sequence[i-2]);
+    }
+
+    return sequence[n];
 };
 
 // Do not edit below this line
