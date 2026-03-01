093b85a664915017c078bcc86860a3354b5c019b
Sarah-Turner <81490363+Sarah-Turner@users.noreply.github.com>
2026-03-01 11:14:06 -0800

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let currAge;
+        if (!person["yearOfDeath"]) {
+            let currYear = (new Date()).getFullYear();
+            currAge = currYear - person.yearOfBirth;
+        } else {
+            currAge = person.yearOfDeath - person.yearOfBirth;
+        }
+        let oldestAge;
+        if (!oldest["yearOfDeath"]) {
+            let currYear = (new Date()).getFullYear();
+            oldestAge = currYear - oldest.yearOfBirth;
+        } else {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
 
+        if (currAge > oldestAge) {
+            return person;
+        } else {
+            return oldest;
+        }
+    })
 };
 
 // Do not edit below this line
